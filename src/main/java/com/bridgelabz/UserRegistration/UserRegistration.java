fcafe29399eb50154a41e08eb22aafea2d0d6289
ankushmalode <you@example.com>
2021-10-23 01:10:38 +0530

    validate user's last name

--- src/main/java/com/bridgelabz/UserRegistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration/UserRegistration.java
@@ -13,7 +13,15 @@ public class UserRegistration {
 
 class UserRegistrationDetails {
     String firstName;
+    String lastName;
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
     public String getFirstName() {
         return firstName;
     }
@@ -30,7 +38,7 @@ class UserRegistrationImpl {
      * The name should at-least contain 3 letters
      */
 
-    public boolean validateFirstName(String firstName) {
+    public boolean validateName(String firstName) {
         String regexName = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regexName);
 
@@ -46,11 +54,21 @@ class UserRegistrationImpl {
         UserRegistrationDetails userRegistrationDetails = new UserRegistrationDetails();
         UserRegistrationImpl userRegistration = new UserRegistrationImpl();
 
-        System.out.println("Enter your first name ");
+        System.out.print("Enter your first name: ");
         userRegistrationDetails.setFirstName(scanner.next());
 
-        boolean flag = userRegistration.validateFirstName(userRegistrationDetails.getFirstName());
-        if (flag) {
+        boolean flag = userRegistration.validateName(userRegistrationDetails.getFirstName());
+        if (flag)
+            System.out.print("");
+         else {
+            System.out.println("Invalid input");
+        }
+
+        System.out.print("Enter your last name: ");
+        userRegistrationDetails.setLastName(scanner.next());
+
+        boolean flagL = userRegistration.validateName(userRegistrationDetails.getLastName());
+        if (flagL) {
             System.out.println("Valid input");
         } else {
             System.out.println("Invalid input");
